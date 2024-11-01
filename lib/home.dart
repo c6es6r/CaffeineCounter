1d93f5ed939e6a1aa683a42c592dc6d2b7d6a86a
c6es6r <julius.meyers@cusgymerk.onmicrosoft.com>
2024-11-01 15:41:06 +0100

    almost working

--- lib/home.dart
+++ lib/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class Home extends StatefulWidget {
   const Home({super.key});
@@ -9,11 +10,64 @@ class Home extends StatefulWidget {
 
 class _HomeState extends State<Home> {
   int caffeineAmount = 0;
+  int selectedAmount = 0;
+  String selectedBeverage = "";
+
+  Color textColor = Colors.black;
+
+  List<String> consumedBeverages = [];
+  String consumedBeveragesString = "";
+
+  @override
+  void initState() {
+    loadData();
+    super.initState();
+  }
 
   void increaseCaffeineAmount(int amount) {
     setState(() {
       caffeineAmount += amount;
+      saveCaffeineAmount(caffeineAmount);
+      if (caffeineAmount > 400) {
+        textColor = Colors.red;
+      }
+      editConsumedBeveragesString();
     });
+  }
+
+  void editConsumedBeveragesString() {
+    consumedBeveragesString = consumedBeverages?.join("\n") ?? "";
+  }
+
+  void loadData() async {
+    final SharedPreferences preferences = await SharedPreferences.getInstance();
+
+    setState(() async {
+      caffeineAmount = preferences.getInt("caffeineAmount") ?? 0;
+      if (await isNewDay()) {
+        // Update your variable here
+        caffeineAmount = 0;
+        preferences.setInt('lastUpdatedDay', DateTime.now().day);
+      }
+    });
+  }
+
+  Future<bool> isNewDay() async {
+    final SharedPreferences preferences = await SharedPreferences.getInstance();
+    final now = DateTime.now();
+    final lastUpdated = preferences.getInt('lastUpdatedDay');
+
+    if (lastUpdated == null) {
+      preferences.setInt('lastUpdatedDay', now.day);
+      return true;
+    }
+
+    return now.day != lastUpdated;
+  }
+
+  void saveCaffeineAmount(int amount) async {
+    final SharedPreferences preferences = await SharedPreferences.getInstance();
+    preferences.setInt("caffeineAmount", amount);
   }
 
   @override
@@ -21,20 +75,60 @@ class _HomeState extends State<Home> {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text("Today's Caffeine Intake: $caffeineAmount mg"),
+        title: Text(
+          "Heute: $caffeineAmount mg",
+          style: TextStyle(color: textColor),
+        ),
       ),
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            ElevatedButton(
-              onPressed: () {
-                increaseCaffeineAmount(100); // Adjust the amount as needed
+          child: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          Text(consumedBeveragesString),
+          Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+            DropdownMenu(
+              dropdownMenuEntries: const <DropdownMenuEntry>[
+                DropdownMenuEntry(value: 65, label: "Espresso"),
+                DropdownMenuEntry(value: 75, label: "Kaffee Tasse"),
+                DropdownMenuEntry(value: 150, label: "Kaffee Becher"),
+                DropdownMenuEntry(value: 160, label: "Monster"),
+                DropdownMenuEntry(value: 80, label: "Red Bull"),
+              ],
+              hintText: "Getränk...",
+              onSelected: (value) {
+                selectedAmount = value;
+                selectedBeverage = value.label;
               },
-              child: const Text('Add 100mg Caffeine'),
+              width: 300,
+              enableFilter: true,
+              enableSearch: true,
             ),
-          ],
-        ),
+            TextButton(
+              onPressed: () {
+                increaseCaffeineAmount(selectedAmount);
+                consumedBeverages.add(selectedBeverage);
+              },
+              child: const Icon(Icons.add),
+            ),
+          ]),
+          const SizedBox(
+            height: 25,
+          )
+        ],
+      )),
+      bottomNavigationBar: NavigationBar(
+        indicatorColor: Colors.deepPurple,
+        destinations: const <Widget>[
+          NavigationDestination(
+            selectedIcon: Icon(Icons.home),
+            icon: Icon(Icons.home_outlined),
+            label: 'Home',
+          ),
+          NavigationDestination(
+            icon: Badge(child: Icon(Icons.book_rounded)),
+            label: 'Aufzeichnungen',
+          ),
+        ],
       ),
     );
   }
